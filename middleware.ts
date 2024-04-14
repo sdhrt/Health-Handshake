21b1a37ae44f92a8ee525430af6bef748f2e5f5c
Siddhartha Shrestha <sdhrtivv@gmail.com>
2024-04-14 23:42:39 +0545

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,23 @@
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+    function middleware(req) {
+        const token = getToken({ req })
+        if (!!token) {
+            return NextResponse.redirect(
+                `${req.nextUrl.origin}`
+            )
+        }
+    },
+    {
+        pages: {
+            signIn: "/auth/signin",
+        },
+    }
+)
 
 export const config = {
     matcher: ["/dashboard"],
