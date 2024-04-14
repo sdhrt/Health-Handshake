ffcbdb8d0d86180de76ee79bc6d07209056842f0
Siddhartha Shrestha <sdhrtivv@gmail.com>
2024-04-15 00:07:37 +0545

    new middleware test

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,17 +1,10 @@
-import { getServerSession } from "next-auth"
 import Navbar from "./_components/Navbar"
-import { authOptions } from "@/lib/auth"
-import { redirect } from "next/navigation"
 
 async function layout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const session = await getServerSession(authOptions)
-    if (session == null) {
-        redirect("/auth/signin")
-    }
     return (
         <>
             <Navbar />
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
-import { getToken } from "next-auth/jwt"
+import { getServerSession } from "next-auth"
 import { NextRequest, NextResponse } from "next/server"
-
+import { authOptions } from "./lib/auth"
 export { default } from "next-auth/middleware"
 
 export const config = { matcher: ["/dashboard/:path*"] }
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request })
-
-    if (!token) {
+    const session = getServerSession(authOptions)
+    if (session == null) {
         return NextResponse.redirect(
-            `${request.nextUrl.origin}/auth/signin`
+            new URL("/auth/signin", request.url)
         )
     }
 }
