19c69f5e3c1b666049a43abb18d9fc068d39a598
Siddhartha Shrestha <sdhrtivv@gmail.com>
2024-04-14 23:34:10 +0545

    middleware changed

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,5 @@
-import { getToken } from "next-auth/jwt"
-import { NextRequest, NextResponse } from "next/server"
-
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard/:path*"] }
-
-export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request })
-    const isAuth = !!token
-
-    const { pathname } = request.nextUrl
-
-    if (isAuth) {
-        return NextResponse.next()
-    } else {
-        if (pathname.startsWith("/dashboard")) {
-            // Redirect to login (or a custom unauthorized page):
-            return NextResponse.redirect(
-                new URL("/auth/signin", request.url)
-            )
-        }
-    }
+export const config = {
+    matcher: ["/dashboard"],
 }
